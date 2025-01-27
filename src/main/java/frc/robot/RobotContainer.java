8e8527ede28e2f3cdd0c8ac87121651a1182e2e1
Zane Farner <jackie@balikbayanbots.com>
2025-01-26 16:41:45 -0800

    Refined pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,9 +80,9 @@ public class RobotContainer {
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
         joystick.rightBumper().whileTrue(new RunCommand(() -> outtake.set(.3))).whileFalse(new RunCommand(() -> outtake.set(0)));
-        joystick.povUp().whileTrue(new RunCommand(() -> arm.setSpeed(0.25))).whileFalse(new RunCommand(() -> arm.stop()));
-      joystick.povDown().whileTrue(new RunCommand(() -> arm.setSpeed(-0.25))).whileFalse(new RunCommand(() -> arm.stop()));
-        drivetrain.registerTelemetry(logger::telemeterize);
+        joystick.povUp().whileTrue(new RunCommand(() -> arm.setSpeed(0.25))).onFalse(new RunCommand(() -> arm.stop()));
+      joystick.povDown().whileTrue(new RunCommand(() -> arm.setSpeed(-0.25))).onFalse(new RunCommand(() -> arm.stop()));
+      drivetrain.registerTelemetry(logger::telemeterize);
         SmartDashboard.putData(arm);
     }
 
